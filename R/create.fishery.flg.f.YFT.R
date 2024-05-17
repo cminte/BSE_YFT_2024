d8a43e41d75dbcff54ee95e5e04a5f150b783a55
Carolina Minte-Vera <cminte@iattc.org>
2024-05-17 16:17:07 -0700

    Add files via upload
    New fishery definitions for all PS based on "core" areas for DEL fisheries

--- R/create.fishery.flg.f.YFT.R
+++ R/create.fishery.flg.f.YFT.R
@@ -14,28 +14,35 @@ create.fishery.flg.f.YFT = function(strat.defns,PS)
   # compute quarter id vector
   quarter<-ceiling(strat.defns$month/3)
   
-  # YFT SAC 12 assessment
+  # YFT SAC 15 assessment (2024)
   
   if(PS=="DEL") {
     print("Using fishery stratification: YFT SAC 15 for DEL")
     areagear<-rep(NA,nrecs)
-    areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area<=5]<-"FO.A1"
-    areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area>5]<-"FO.A2"
+    areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area=1]<-"FO.A1"
+    areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area=2]<-"FO.A2"
+    areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area=3]<-"FO.A3"
+    areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area=4]<-"FO.A4"
+    areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area=5]<-"FO.A5"
     
-    areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area<=5]<-"UN.A1"
-    areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area>5]<-"UN.A2"
+    
+    areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area=1]<-"UN.A1"
+    areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area=2]<-"UN.A2"
+    areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area=3]<-"UN.A3"
+    areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area=4]<-"UN.A4"
+    areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area=5]<-"UN.A5"
+    
+    
     
     areagear[(strat.defns$gear==4 | strat.defns$gear==7) & strat.defns$area==1]<-"DP.A1"
     areagear[(strat.defns$gear==4 | strat.defns$gear==7) & strat.defns$area==2]<-"DP.A2"
     areagear[(strat.defns$gear==4 | strat.defns$gear==7) & strat.defns$area==3]<-"DP.A3"
     areagear[(strat.defns$gear==4 | strat.defns$gear==7) & strat.defns$area==4]<-"DP.A4"
     areagear[(strat.defns$gear==4 | strat.defns$gear==7) & strat.defns$area==5]<-"DP.A5"
-    #areagear[(strat.defns$gear==4 | strat.defns$gear==7) & strat.defns$area==6]<-"DP.A6"
-    #areagear[(strat.defns$gear==4 | strat.defns$gear==7) & strat.defns$area==7]<-"DP.A7"
-  }
+    }
   
   if(PS=="NOA") {
-    print("Using fishery stratification: YFT SAC 15 for UNA")
+    print("Using fishery stratification: YFT SAC 14 for UNA based on DEL")
     areagear<-rep(NA,nrecs)
     areagear[(strat.defns$gear==2 | strat.defns$gear==5) & (strat.defns$area==1 | strat.defns$area==3)]<-"FO.A1"
     areagear[(strat.defns$gear==2 | strat.defns$gear==5) & (strat.defns$area==2 | strat.defns$area==4)]<-"FO.A2"
@@ -43,20 +50,23 @@ create.fishery.flg.f.YFT = function(strat.defns,PS)
     areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area==1]<-"UN.A1"
     areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area==2]<-"UN.A2"
     areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area==3]<-"UN.A3"
-    #areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area==4]<-"UN.A4"
+    areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area==4]<-"UN.A4"
 
     areagear[(strat.defns$gear==4 | strat.defns$gear==7) & (strat.defns$area==1 | strat.defns$area==3)]<-"DP.A1"
     areagear[(strat.defns$gear==4 | strat.defns$gear==7) & (strat.defns$area==2 | strat.defns$area==4)]<-"DP.A2"
   }
 
   if(PS=="OBJ") {
-    print("Using fishery stratification: YFT SAC 15 for OBJ")
+    print("Using fishery stratification: YFT SAC 15 for OBJ based on DEL")
     areagear<-rep(NA,nrecs)
     areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area==1]<-"FO.A1"
     areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area==2]<-"FO.A2"
     areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area==3]<-"FO.A3"
     areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area==4]<-"FO.A4"
     areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area==5]<-"FO.A5"
+    areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area==6]<-"FO.A6"
+    areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area==7]<-"FO.A7"
+    
 
     areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area<5]<-"UN.A1"
     areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area==5]<-"UN.A2"
--- R/create.strat.flg.f.YFT.R
+++ R/create.strat.flg.f.YFT.R
@@ -5,8 +5,12 @@
 #' @export
 
 create.strat.flg.f.YFT = function(lat.5deg,lon.5deg,is.lwrght,month,setype,vessel.class,PS) {
+  
+  # revisited on 05/17/2024 to get catches for the core DEL area
+  # for all set types
   # fishery definitions updated with the 2024 SAC15 areas
   # by CV Minte-Vera  3/31/2024
+  # for all sets
   # number of records in input vectors
   nrecs<-length(lat.5deg)
   
@@ -30,55 +34,66 @@ create.strat.flg.f.YFT = function(lat.5deg,lon.5deg,is.lwrght,month,setype,vesse
   # Catch areas for YFT in DEL
   if(PS=="DEL") {
   print("Using catch stratification: YFT SAC 15 DEL")
+  # 3/31/2024 - OK
   area<-rep(1,nrecs)
   area[lon.5deg>=( -92.5) & lat.5deg<=(2.5) ]<-2
   area[lon.5deg<=(-128.5) & lat.5deg>(2.5)]<-3
   area[lon.5deg>(-128.5) & lat.5deg>(2.5) & lat.5deg<=(17.5)]<-4
-  area[lon.5deg>(-128.5)&lat.5deg>=(22.5)]<-5
-    
-  #print("Using catch stratification: YFT SAC 12 DEL")
-  #area<-rep(1,nrecs)
-  #area[lon.5deg>(-132.5) & lon.5deg<=(-122.5) & lat.5deg>(2.5)]<-2
-  #area[lat.5deg>(17.5) & lon.5deg>(-122.5)]<-3
-  #area[lat.5deg>(7.5) & lat.5deg<=(17.5) & lon.5deg>(-122.5)]<-4
-  #area[lat.5deg>(-2.5) & lat.5deg<=(7.5) & lon.5deg>(-122.5)]<-5
-  #area[lat.5deg<=(2.5) & lon.5deg<=(-97.5)]<-6
-  #area[lat.5deg<=(2.5) & lon.5deg>(-97.5)]<-7
+  area[lon.5deg>(-128.5)&lat.5deg>(17.5)]<-5
+
+  # 2020  areas (7 areas in total)
+  # area[lon.5deg>(-132.5) & lon.5deg<=(-122.5) & lat.5deg>(2.5)]<-2
+  # area[lat.5deg>(17.5) & lon.5deg>(-122.5)]<-3
+  # area[lat.5deg>(7.5) & lat.5deg<=(17.5) & lon.5deg>(-122.5)]<-4
+  # area[lat.5deg>(-2.5) & lat.5deg<=(7.5) & lon.5deg>(-122.5)]<-5
+  # area[lat.5deg<=(2.5) & lon.5deg<=(-97.5)]<-6
+  # area[lat.5deg<=(2.5) & lon.5deg>(-97.5)]<-7
   
   # Fishery area-gears for YFT in DEL (UNA and OBJ are junk)
   print("Using fishery stratification: YFT SAC 15 DEL")
   fishery.areagear<-rep(NA,nrecs)
-  fishery.areagear[(gear==2 | gear==5) & area<=5]<-"FO.A1"
-  fishery.areagear[(gear==2 | gear==5) & area>5]<-"FO.A2"
+  fishery.areagear[(gear==2 | gear==5) & area==1]<-"FO.A1"
+  fishery.areagear[(gear==2 | gear==5) & area==2]<-"FO.A2"
+  fishery.areagear[(gear==2 | gear==5) & area==3]<-"FO.A3"
+  fishery.areagear[(gear==2 | gear==5) & area==4]<-"FO.A4"
+  fishery.areagear[(gear==2 | gear==5) & area==5]<-"FO.A5"
   
-  fishery.areagear[(gear==3 | gear==6) & area<=5]<-"UN.A1"
-  fishery.areagear[(gear==3 | gear==6) & area>5]<-"UN.A2"
+  fishery.areagear[(gear==3 | gear==6) & area==1]<-"UN.A1"
+  fishery.areagear[(gear==3 | gear==6) & area==2]<-"UN.A2"
+  fishery.areagear[(gear==3 | gear==6) & area==3]<-"UN.A3"
+  fishery.areagear[(gear==3 | gear==6) & area==4]<-"UN.A4"
+  fishery.areagear[(gear==3 | gear==6) & area==5]<-"UN.A5"
+  
   
   fishery.areagear[(gear==4 | gear==7) & area==1]<-"DP.A1"
   fishery.areagear[(gear==4 | gear==7) & area==2]<-"DP.A2"
   fishery.areagear[(gear==4 | gear==7) & area==3]<-"DP.A3"
   fishery.areagear[(gear==4 | gear==7) & area==4]<-"DP.A4"
   fishery.areagear[(gear==4 | gear==7) & area==5]<-"DP.A5"
-  #fishery.areagear[(gear==4 | gear==7) & area==6]<-"DP.A6"
-  #fishery.areagear[(gear==4 | gear==7) & area==7]<-"DP.A7"
-  
   }
 
-  # Catch areas for YFT in UNA
+  # Catch areas for YFT in UNA 
+  # 3/31/2024 - OK
   if(PS=="NOA") {
-  print("Using catch stratification: YFT SAC 15 UNA")
-  area<-rep(1,nrecs)
-  area[lon.5deg>(-130) & lat.5deg>=5]<-3
-  area[lon.5deg>(-130) & lat.5deg<5]<-2
-    
-  #print("Using catch stratification: YFT SAC 12 UNA")
-  #area<-rep(2,nrecs)
-  #area[lat.5deg>(12.5)]<-1
-  #area[lat.5deg>(2.5) & lat.5deg<=(12.5) & lon.5deg>(-117.5)]<-3
-  #area[lat.5deg<=(2.5) & lon.5deg>(-87.5)]<-4
+  print("Using catch stratification: YFT SAC 15 UNA based on DEL")
   
+    area<-rep(1,nrecs)
+    area[lon.5deg>(-128.5) & lat.5deg<5]<-2
+    area[lon.5deg>(-128.5) & lat.5deg>=5 & lat.5deg<=(17.5)]<-3
+    area[lon.5deg>(-128.5)&lat.5deg>(17.5)]<-4
+  
+  #2020 areas
+  # area<-rep(2,nrecs)
+  # area[lat.5deg>(12.5)]<-1
+  # area[lat.5deg>(2.5) & lat.5deg<=(12.5) & lon.5deg>(-117.5)]<-3
+  # area[lat.5deg<=(2.5) & lon.5deg>(-87.5)]<-4
+  # 
+    
   # Fishery area-gears for YFT in UNA (DEL and OBJ are junk)
-  print("Using fishery stratification: YFT SAC 15 UNA")
+  #print("Using fishery stratification: YFT SAC 12 UNA")
+  print("Using catch stratification: YFT SAC 15 UNA based on DEL")
+  
+  
   fishery.areagear<-rep(NA,nrecs)
   fishery.areagear[(gear==2 | gear==5) & (area==1 | area==3)]<-"FO.A1"
   fishery.areagear[(gear==2 | gear==5) & (area==2 | area==4)]<-"FO.A2"
@@ -86,7 +101,7 @@ create.strat.flg.f.YFT = function(lat.5deg,lon.5deg,is.lwrght,month,setype,vesse
   fishery.areagear[(gear==3 | gear==6) & area==1]<-"UN.A1"
   fishery.areagear[(gear==3 | gear==6) & area==2]<-"UN.A2"
   fishery.areagear[(gear==3 | gear==6) & area==3]<-"UN.A3"
-  #fishery.areagear[(gear==3 | gear==6) & area==4]<-"UN.A4"
+  fishery.areagear[(gear==3 | gear==6) & area==4]<-"UN.A4"
   
   fishery.areagear[(gear==4 | gear==7) & (area==1 | area==3)]<-"DP.A1"
   fishery.areagear[(gear==4 | gear==7) & (area==2 | area==4)]<-"DP.A2"
@@ -94,31 +109,37 @@ create.strat.flg.f.YFT = function(lat.5deg,lon.5deg,is.lwrght,month,setype,vesse
   }
   
   # Catch areas for YFT in OBJ
+  # 3/31/2024 - OK
   if(PS=="OBJ") {
-  print("Using catch stratification: YFT SAC 15 OBJ")
-  area<-rep(1,nrecs)
-  area[lon.5deg>(-130) & lat.5deg<=(-10)]<-2
-  area[lon.5deg>(-130) & lat.5deg<=0 & lat.5deg>(-10)]<-3
-  #area[lon.5deg>(-130) &  lat.5deg>0]<-4
-  
-  area[lon.5deg>(-130) & lon.5deg<=(-110) & lat.5deg>0]<-4
-  area[lon.5deg>(-110) & lat.5deg>0]<-5
-  
+  print("Using catch stratification: YFT SAC 15 OBJ based on DEL")
+    area<-rep(1,nrecs)
+    area[lon.5deg>(-128.5) & lat.5deg<=(-10)]<-2
+    area[lon.5deg>(-128.5) & lat.5deg<=0 & lat.5deg>(-10)]<-3
+    area[lon.5deg>(-128.5) & lon.5deg<=(-110) & lat.5deg>0 & lat.5deg<=(2.5)]<-4
+    area[lon.5deg>(-110) & lat.5deg>0 & lat.5deg<=(2.5)]<-5
+    area[lon.5deg>(-110) & lat.5deg>(2.5)]<-7
+    area[lon.5deg>(-128.5) & lon.5deg<=(-110) & lat.5deg>(2.5)]<-6
   #print("Using catch stratification: YFT SAC 12 OBJ")
-  #area<-rep(2,nrecs)
-  #area[lon.5deg<=(-97.5) & lat.5deg>(-2.5)]<-1
-  #area[lat.5deg>(-12.5) & lat.5deg<=(-2.5) & lon.5deg<=(-97.5)]<-3
-  #area[lat.5deg>(-12.5) & lat.5deg<=(-2.5) & lon.5deg>(-97.5)]<-4
-  #area[lat.5deg<=(-12.5)]<-5
-  
-  # Fishery area-gears for YFT in OBJ (DEL and UNA are junk)
-  print("Using fishery stratification: YFT SAC 15 OBJ")
+  # 2020 
+  # area<-rep(2,nrecs)
+  # area[lon.5deg<=(-97.5) & lat.5deg>(-2.5)]<-1
+  # area[lat.5deg>(-12.5) & lat.5deg<=(-2.5) & lon.5deg<=(-97.5)]<-3
+  # area[lat.5deg>(-12.5) & lat.5deg<=(-2.5) & lon.5deg>(-97.5)]<-4
+  # area[lat.5deg<=(-12.5)]<-5
+  # 
+  # # Fishery area-gears for YFT in OBJ (DEL and UNA are junk)
+  print("Using catch stratification: YFT SAC 15 OBJ based on DEL")
+   
+   
+  #print("Using fishery stratification: YFT SAC 12 OBJ")
   fishery.areagear<-rep(NA,nrecs)
   fishery.areagear[(gear==2 | gear==5) & area==1]<-"FO.A1"
   fishery.areagear[(gear==2 | gear==5) & area==2]<-"FO.A2"
   fishery.areagear[(gear==2 | gear==5) & area==3]<-"FO.A3"
   fishery.areagear[(gear==2 | gear==5) & area==4]<-"FO.A4"
   fishery.areagear[(gear==2 | gear==5) & area==5]<-"FO.A5"
+  fishery.areagear[(gear==2 | gear==5) & area==6]<-"FO.A6"
+  fishery.areagear[(gear==2 | gear==5) & area==6]<-"FO.A7"
   
   fishery.areagear[(gear==3 | gear==6) & area<5]<-"UN.A1"
   fishery.areagear[(gear==3 | gear==6) & area==5]<-"UN.A2"
