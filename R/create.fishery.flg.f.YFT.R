53df326e266d81bc25a3f2092fdf26a47b1c4629
Carolina Minte-Vera <cminte@iattc.org>
2024-05-17 16:25:43 -0700

    Update create.fishery.flg.f.YFT.R
    correction of "="  to "=="

--- R/create.fishery.flg.f.YFT.R
+++ R/create.fishery.flg.f.YFT.R
@@ -19,18 +19,18 @@ create.fishery.flg.f.YFT = function(strat.defns,PS)
   if(PS=="DEL") {
     print("Using fishery stratification: YFT SAC 15 for DEL")
     areagear<-rep(NA,nrecs)
-    areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area=1]<-"FO.A1"
-    areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area=2]<-"FO.A2"
-    areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area=3]<-"FO.A3"
-    areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area=4]<-"FO.A4"
-    areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area=5]<-"FO.A5"
+    areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area==1]<-"FO.A1"
+    areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area==2]<-"FO.A2"
+    areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area==3]<-"FO.A3"
+    areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area==4]<-"FO.A4"
+    areagear[(strat.defns$gear==2 | strat.defns$gear==5) & strat.defns$area==5]<-"FO.A5"
     
     
-    areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area=1]<-"UN.A1"
-    areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area=2]<-"UN.A2"
-    areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area=3]<-"UN.A3"
-    areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area=4]<-"UN.A4"
-    areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area=5]<-"UN.A5"
+    areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area==1]<-"UN.A1"
+    areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area==2]<-"UN.A2"
+    areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area==3]<-"UN.A3"
+    areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area==4]<-"UN.A4"
+    areagear[(strat.defns$gear==3 | strat.defns$gear==6) & strat.defns$area==5]<-"UN.A5"
     
     
     
